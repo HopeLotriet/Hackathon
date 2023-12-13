1b5b48b8134b9a32ccdb15a7364da4a33ac5d4aa
AmoMonnanyana <amogelangmonnanyana@gmail.com>
2023-12-13 14:23:30 +0200

    page modified

--- inventory_system/accounts/admin.py
+++ inventory_system/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Inventory
+from .models import Inventory, Order
 
 # Register your models here.
 admin.site.register(Inventory)
+admin.site.register(Order)
 
--- inventory_system/accounts/urls.py
+++ inventory_system/accounts/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from django.contrib import admin
 from . import views
-from .views import inventory, per_product, update, delete, add_product, dashboard, order_list, create_order, update_order_status, order_homepage
+from .views import inventory, per_product, update, delete, add_product, dashboard, order_list
 
 
 
@@ -25,7 +25,4 @@ urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path('about/', views.about, name='about'),
     path('order_list', order_list, name='order_list'),
-    path('create_order', create_order, name='create_order'),
-    path('update_order_status/<int:order_id>/', update_order_status, name='update_order_status'),
-    path('orders', order_homepage, name='order_homepage'),
 ]
