b0bf4d2a33e19fb672accff90306870b5963dab9
Xolani Sibisi <xolansibisi@gmail.com>
2023-12-13 11:56:31 +0200

    updating product update

--- inventory_system/accounts/views.py
+++ inventory_system/accounts/views.py
@@ -137,7 +137,7 @@ def update(request, pk):
             inventory.sales = float(inventory.cost_per_item) * float(inventory.quantity_sold)
             inventory.save()
             messages.success(request, "Update Successful")
-            return redirect(f"/inventory/per_product/{pk}/")
+            return redirect(f"/per_product/{pk}/")
     else:
         updateForm = InventoryUpdateForm(instance=inventory)
 
