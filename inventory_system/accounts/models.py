982c76e32dd4572ba83659386d75de3b18640abe
AmoMonnanyana <amogelangmonnanyana@gmail.com>
2023-12-21 13:38:00 +0200

    order history

--- inventory_system/accounts/models.py
+++ inventory_system/accounts/models.py
@@ -154,6 +154,7 @@ class customerOrderHistory(models.Model):
 
     order_id = models.CharField(max_length=100, default="")
     order_date = models.DateTimeField(auto_now_add=True)
+    customer = models.CharField(max_length=100, default="")
     product = models.CharField(max_length=100, default="")
     quantity_ordered = models.PositiveIntegerField(null=True)
     amount_spent = models.DecimalField(max_digits=19, decimal_places=2, null=True, blank=True)
