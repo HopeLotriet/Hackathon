3d3ad13e9b489b11c7cf279c6a739993fc16772e
Xolani Sibisi <xolansibisi@gmail.com>
2024-01-29 17:19:35 +0200

    Last change

--- inventory_system/inventory_system/wsgi.py
+++ inventory_system/inventory_system/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = "{{inventory_system}}.settings"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_system.settings')
 
 application = get_wsgi_application()
+
 
